b743acb31c20f9e401328272e81fc9af8fdf0e47
Gerardo Ponce <gerardoponcepari@gmail.com>
2021-06-17 10:30:52 -0500

    Third commit

--- Snapchat/Controller/ImageViewController.swift
+++ Snapchat/Controller/ImageViewController.swift
@@ -28,8 +28,6 @@ class ImageViewController: UIViewController, UIImagePickerControllerDelegate, UI
         imageView.image = image
         imageView.backgroundColor = UIColor.clear
         imagePicker.dismiss(animated: true, completion: nil)
-        //Subir imagen
-        uploadToCLoud(fileURL: url)
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
@@ -40,35 +38,21 @@ class ImageViewController: UIViewController, UIImagePickerControllerDelegate, UI
     
     @IBAction func chooseContactTapped(_ sender: UIButton) {
         chooseContactButton.isEnabled = true
+        let imageData = imageView.image!.pngData()!
+        let storageReference = Storage.storage()
+        let imageFolder = storageReference.reference().child("images")
+
+        imageFolder.putData(imageData, metadata: nil){metadata, error in
+            imageFolder.downloadURL {url, error in
+                guard let url = url else {return}
+
+            }
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let siguienteVC = segue.destination as! ChooseContactViewController
-        siguienteVC.imageURL = sender as! String // Sale nulo el sender
+        //siguienteVC.imageURL = sender as! String
         siguienteVC.descrip = descriptionTextField.text!
-    }
-    
-    func uploadToCLoud(fileURL: URL){
-        
-        let imagenesFolder = Storage.storage().reference().child("images")
-        let imagenData = imageView.image!.pngData()! as NSData
-        
-        imagenesFolder.child("\(NSUUID().uuidString).jpg").putFile(from: fileURL, metadata: nil, completion: {(metadata, error) in
-            print("intentando subir la imagen...")
-            self.chooseContactButton.isEnabled = false
-            if error != nil {
-                print("Ocurrió un error:\(error)")
-            } else {
-                print("Imagen subida")
-                self.chooseContactButton.isEnabled = true
-                self.performSegue(withIdentifier: "chooseContactSegue", sender: metadata?.storageReference?.downloadURL(completion:{(fileURL, error) in
-                    if error != nil {
-                        print(error?.localizedDescription)
-                        return
-                    }
-                    let downloadURL = fileURL?.absoluteString
-                }))
-            }
-        })
     }
 }
